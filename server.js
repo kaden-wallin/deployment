6cfb8e8091ff17ea06f13b15f9828c741394bfb8
BuildTools <unconfigured@null.spigotmc.org>
2022-12-14 20:05:51 -0700

    Added .env file and made sure server is running

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-app.listen(3000)
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
 
 module.exports = app;
